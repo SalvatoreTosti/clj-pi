30a73146b608235416490812cf435ba22e6fe6ac
Salvatore Tosti <saltosti@gmail.com>
2017-06-11 17:16:43 -0700

    Created basic pi generation functions
    Based on Bailey–Borwein–Plouffe formula.

--- src/clj_pi/core.clj
+++ src/clj_pi/core.clj
@@ -1,7 +1,24 @@
 (ns clj-pi.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn sum-component
+  [i numer denom-add]
+   (with-precision (+ i 10) (/ numer (+ denom-add (* 8 i)))))
+
+(defn sub-component
+  [i]
+    (-> (sum-component i 4M 1)
+        (- (sum-component i 2M 4))
+        (- (sum-component i 1M 5))
+        (- (sum-component i 1M 6))))
+
+(defn pi-nth
+  [n]
+  (-> (reduce + (map (fn [i]  (with-precision (+ i 10) (*
+                                                      (with-precision (+ i 10) (/ 1  (with-precision (+ i 10) (.pow (bigdec 16) i))))
+                                                      (sub-component i)))) (range 0 100)))
+    (str)
+    (subs 2)
+    (#(str "3" %))
+    (nth (dec n))
+    (str)))
